2a7aef360273ae12f003f3b702d46806f77842a4
github username <github email address>
2020-10-18 15:57:31 -0400

    enable sessions + create show route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    #enable the session hash 
+    enable :sessions
+    #set a session secret for an extra layer of security 
+    set :session_secret, "coupon_xtremesuper_secret_session"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,20 @@ class UsersController < ApplicationController
         user = User.find_by(email: params[:email])
         #authenticate the user 
        if user && user.authenticate(params[:password])
-            #log them in 
+            #creating a key/value pair in the session hash using the user id to log them in 
+            session[:user_id] = user.id 
             #redirect user's profile(users show) 
-            
+            #dynamic route = id in session
+            redirect "/users/#{user.id}"
        else
             #show an error message 
             redirect '/login'
-            
         end 
     end
+
+    #user show route
+    get '/users/:id' do 
+        "users show page!"
+    end 
 
 end 
