e65878c3e28ca00e37d9f96ba851dab58eb29398
github username <github email address>
2020-10-19 23:16:21 -0400

    added logout link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else 
+      erb :welcome
+    end 
   end
 
   helpers do 
     #keeps track of the login user
     def current_user 
       User.find_by(id: session[:user_id])
+    end 
+    #returns a boolean if the user is logged in or not 
+    #double bang converts turthy/falsy to hard true/false
+    def logged_in?
+      !!current_user
     end 
   end 
 end
