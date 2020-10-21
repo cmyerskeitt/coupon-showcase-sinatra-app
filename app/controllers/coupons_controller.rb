82e3ee22f723602cc11b720b325c778837c1d089
github username <github email address>
2020-10-21 19:56:42 -0400

    finished create functionality

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,10 +9,17 @@ class CouponsController < ApplicationController
         erb :'/coupons/new'
     end 
 
+    post '/coupons' do 
+        coupon = Coupon.create(brand: params[:brand], amount: params[:amount], expiration: params[:expiration], description: params[:description], user_id: current_user.id)
+        redirect "/coupons/#{coupon.id}"
+    end 
+
     get '/coupons/:id' do
         @coupons = Coupon.find_by(params[:id])
         # binding.pry
         erb :'/coupons/show'
     end
+
+  
 
 end 
