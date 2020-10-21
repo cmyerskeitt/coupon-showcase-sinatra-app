09068854d8544a64ccd064e98cfe10fdab0c9cea
github username <github email address>
2020-10-20 21:39:52 -0400

    create how route for individual post

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,11 +1,13 @@
 class CouponsController < ApplicationController 
 
-get '/coupons' do
-    @coupons = Coupon.all
-    erb :'coupons/index'
-end
+    get '/coupons' do
+        @coupons = Coupon.all
+        erb :'coupons/index'
+    end
 
-
-
+    get '/coupons/:id' do
+        @coupons = Coupon.find(params[:id])
+        erb :'/coupons/show'
+    end
 
 end 
